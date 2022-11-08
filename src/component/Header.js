375c622d59295b37ac9a9807a6db2c7e26fbbdc7
elson_gasa <elson.gasa.ext@neveling.net>
2022-11-08 10:55:48 +0100

    added changes

---
+++ src/component/Header.js
@@ -0,0 +1,28 @@
+import "./Header.css";
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <section className="header">
+        <div className="header-wrapper">
+          <div className="navbar">
+            <ul>
+              <li>
+                <a href="test">Test</a>
+              </li>
+              <li>
+                <a href="test">Test</a>
+              </li>
+              <li>
+                <a href="test">Test</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Header;
