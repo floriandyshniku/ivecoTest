aa038322fd270457e5b352a0859e7868b2e31a5d
Florian <dyshnikuflori@gmail.com>
2022-11-23 17:58:27 +0100

    functionality in vehicles component

--- iveco/src/vehicles/vehicles-header/Vehicle-header.js
+++ iveco/src/vehicles/vehicles-header/Vehicle-header.js
@@ -5,9 +5,7 @@ import React from 'react'
 
 
 function Header2() {
-    const buttonLabel = [
-     "Tutti i veicoli", "Lunghe distanze", "Consegne regionali", "Consegne urbane",
-     "Municipalita", "Costruzioni", "Off Road"]
+    
     
 
   return (
@@ -15,13 +13,6 @@ function Header2() {
         <div className="title-elm">
             <div className="title">
                 <h1 className='header-size'>Veicoli e missioni</h1>
-            </div>
-            <div className="tab-items">
-                    {
-                    buttonLabel.map((label) => {
-                        return <button >{label}</button>
-                    })
-                    }
             </div>
         </div>
     </section>
