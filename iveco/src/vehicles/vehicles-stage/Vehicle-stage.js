6c52c0b3f100e37d4490c89c1b08fefcaddf1bf1
Florian <dyshnikuflori@gmail.com>
2022-12-01 11:45:19 +0100

    update functionality

--- iveco/src/vehicles/vehicles-stage/Vehicle-stage.js
+++ iveco/src/vehicles/vehicles-stage/Vehicle-stage.js
@@ -10,12 +10,10 @@ function Stage2() {
       })
     ),
     "all",
-  ];
+  ]
+
   const [open, setOpen] = useState(false)
   const [items, setItems] = useState(data);
-  let teaser = document.getElementsByClassName("teaser-content")
-   console.log(teaser)
-
 
   const filterItems = (categoryItem) => {
     if (categoryItem === "all") {
@@ -28,13 +26,11 @@ function Stage2() {
     }
   };
   const [index, setIndex] = useState(0)
-  const [active, setActive] = useState(false);
   const handleClick= (open, key) => {
     if(open === false){
         setOpen(true)
     }
     else if(index !== key ){
-      // setOpen(false)
       setOpen(true)
      
     } 
@@ -68,7 +64,8 @@ function Stage2() {
             {items.map((elem, key) => {
                 const { name, image} = elem;
               return (
-                <div className="vehicles-content"  onClick={event => handleClick(open, key)} key={key}>
+                <div className="vehicles-content"  onClick={event => handleClick(open, key)} key={key}
+                style={{borderBottom: open && key === index ? "#1554FF solid 3px" : "none"}}>
                   <img src={image} alt="" />
                   <p>{name}</p>
                   <span className="star-svg">
@@ -79,7 +76,7 @@ function Stage2() {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
+                      <path style={{stroke: open && key === index ? "#1554FF" : "#EEEEEE"}}
                         d="M13.986 7.47434L11.4538 1.98442C11.2751 1.59699 10.7244 1.59699 10.5457 1.98442L8.01347 7.47434C7.94064 7.63224 7.791 7.74096 7.61831 7.76144L1.61459 8.47327C1.19089 8.52351 1.02073 9.0472 1.33398 9.33689L5.77269 13.4417C5.90036 13.5597 5.95752 13.7357 5.92363 13.9062L4.74537 19.8361C4.66222 20.2545 5.1077 20.5782 5.48001 20.3698L10.7555 17.4168C10.9073 17.3319 11.0922 17.3319 11.244 17.4168L16.5195 20.3698C16.8918 20.5782 17.3373 20.2545 17.2541 19.8361L16.0759 13.9062C16.042 13.7357 16.0991 13.5597 16.2268 13.4417L20.6655 9.33689C20.9788 9.0472 20.8086 8.52351 20.3849 8.47327L14.3812 7.76144C14.2085 7.74096 14.0589 7.63224 13.986 7.47434Z"
                         stroke="#EEEEEE"
                         stroke-width="2"
