30836302728414e58c8a35a07e92265ea614b432
Florian <dyshnikuflori@gmail.com>
2022-12-01 14:39:03 +0100

    pagination functionality

--- iveco/src/head/stage/Stage.js
+++ iveco/src/head/stage/Stage.js
@@ -1,4 +1,4 @@
-import React ,{useRef} from 'react'
+import React ,{useRef, useState} from 'react'
 import "./Stage.scss"
 import blueEl from "../../Images/slider-button.png"
 import vehicles from "../../Images/vehicles.png"
@@ -8,10 +8,25 @@ import union from "../../Images/union.png"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
-function Stage() {
-  
-
+const titles = [
+  {
+    id:0,
+    title:"Driver Pal with Alexa"
+  },
+  {
+    id:1,
+    title:"Driver Pal1"
+  },
+  {
+    id:2,
+    title:"Driver Pal2"
+  },
+  {
+    id:3,
+    title:"Driver Pal3"
+  }
+  ]
+  function Stage() {
   const elementRef = useRef(null)
   const scrollBottom = () => {
     elementRef.current.nextSibling.scrollIntoView({
@@ -22,113 +37,51 @@ function Stage() {
 
     });
   }
-
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     fade: true,
     slidesToShow: 1,
-    slidesToScroll: 1,
- 
+    slidesToScroll: 1
   };
+
   return (
     <>
     <section className='stage-wrapper' ref={elementRef}>
     <Slider {...settings}>
-          <div>
-          <div className="slide-container">
-        
-        <div className="page-title-module">
-          <div className="img-wrapper">
-            <img src={blueEl} alt="" />
-          </div>
-          <div className="title-content">
-            <h2>Driver Pal with Alexa</h2>
-            <p>Maximum long description made by two lines of text</p>
-          </div>
-        </div>
-        <div className="iveco-content">
-          <img src={vehicles} alt="" />
-        </div>
-        <div className="images-content">
-          <img src={dailyLogo} alt="" className='images-content'/>
-          <img src={amazonLogo} alt="" />
-        </div>
-        <div className="scroll-el">
-          <img src={union} alt="" onClick={scrollBottom} />
-        </div>
-      </div>
-          </div>
-          <div>
-          <div className="slide-container">
-        <div className="page-title-module">
-          <div className="img-wrapper">
-            <img src={blueEl} alt="" />
-          </div>
-          <div className="title-content">
-            <h2>Driver Pal </h2>
-            <p>Maximum long description made by two lines of text</p>
-          </div>
-        </div>
-        <div className="iveco-content">
-          <img src={vehicles} alt="" />
-        </div>
-        <div className="images-content">
-          <img src={dailyLogo} alt="" className='images-content'/>
-          <img src={amazonLogo} alt="" />
-        </div>
-        <div className="scroll-el">
-          <img src={union} alt="" onClick={scrollBottom}/>
-        </div>
-      </div>
-          </div>
-          <div>
-          <div className="slide-container">
-        <div className="page-title-module">
-          <div className="img-wrapper">
-            <img src={blueEl} alt="" />
-          </div>
-          <div className="title-content">
-            <h2>Lorem ipsum dolor</h2>
-            <p>Maximum long description made by two lines of text</p>
-          </div>
-        </div>
-        <div className="iveco-content">
-          <img src={vehicles} alt="" />
-        </div>
-        <div className="images-content">
-          <img src={dailyLogo} alt="" className='images-content'/>
-          <img src={amazonLogo} alt="" />
-        </div>
-        <div className="scroll-el">
-          <img src={union} alt="" onClick={scrollBottom}/>
-        </div>
-      </div>
-          </div>
-          <div>
-          <div className="slide-container">
-        <div className="page-title-module">
-          <div className="img-wrapper">
-            <img src={blueEl} alt="" />
-          </div>
-          <div className="title-content">
-            <h2>Title</h2>
-            <p>Maximum long description made by two lines of text</p>
-          </div>
-        </div>
-        <div className="iveco-content">
-          <img src={vehicles} alt="" />
-        </div>
-        <div className="images-content">
-          <img src={dailyLogo} alt="" className='images-content'/>
-          <img src={amazonLogo} alt="" />
-        </div>
-        <div className="scroll-el">
-          <img src={union} alt=""onClick={scrollBottom} />
-        </div>
-        </div>
-          </div>
+          {
+            titles.map((elm) => {
+              return (<div className="slide-container">
+              <div className="page-title-module">
+                <div className="img-wrapper">
+                  <img src={blueEl} alt="" />
+                </div>
+                <div className="title-content">
+                  <h2>{elm.title}</h2>
+                  <p>Maximum long description made by two lines of text</p>
+                </div>
+              </div>
+              <div className="iveco-content">
+                <img src={vehicles} alt="" />
+              </div>
+              <div className="images-content">
+                <img src={dailyLogo} alt="" className='images-content'/>
+                <img src={amazonLogo} alt="" />
+                <div className="index-wrapper">
+                <p>{elm.id}</p>
+                <p>/</p>
+                <p>{titles.length}</p>
+                </div>
+              </div>
+              <div className="scroll-el">
+                <img src={union} alt="" onClick={scrollBottom} />
+              </div>
+            </div>)
+            })
+          }
+         
+          
           
         </Slider>
       
